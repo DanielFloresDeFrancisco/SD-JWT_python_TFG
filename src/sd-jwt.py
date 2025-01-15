5e3e9584a3a17dcff0f06a848363309723dc1f8b
DanielFloresDeFrancisco <danflode@alum.us.es>
2025-01-15 17:49:28 +0100

    feat(sd-jwt): adding token class

---
+++ src/sd-jwt.py
@@ -0,0 +1,40 @@
+class Header():
+
+    def __init__(self, alg, typ):
+        
+        self.alg = alg
+        self.typ = typ
+
+
+class Payload():
+
+    def __init__(self, iss, exp, sub, aud, claims):
+
+        self.iss = iss
+        self.exp = exp
+        self.sub = sub
+        self.aud = aud
+        self.claims = claims
+
+
+class Signature():
+
+    def __init__(self, signature_string):
+        
+        self.signature_string = signature_string
+
+
+class SD_JWT():
+
+    def __init__(self, header, payload, signature):
+        
+        self.header = header
+        self.payload = payload
+        self.signature = signature
+
+header = Header("HS256", "JWT")
+payload = Payload(12,13,"Dani","Lumi", [])
+signature = Signature("jknsfvnvdvn4gnir5")
+sdjwt = SD_JWT(header, payload, signature)
+
+print(sdjwt.header.alg + sdjwt.payload.sub)
