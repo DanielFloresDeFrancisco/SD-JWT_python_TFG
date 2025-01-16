2b14cb1ab8845a78e444368e1211a121be982ebc
DanielFloresDeFrancisco <danflode@alum.us.es>
2025-01-16 13:02:29 +0100

    feat(utils): create jwt with HS256 signature

--- src/token_settings.py
+++ src/token_settings.py
@@ -1,13 +1,17 @@
+import os
+import base64
 # General settings for inmutable claims of a SD-JWT
 from time import time, ctime
 # HEADER
-alg = "HS256"
-typ = "JWT"
+alg = '"HS256"'
+typ = '"JWT"'
 
 # PAYLOAD
-aud = "Javier"
-sub = "Daniel"
+aud = '"Javier"'
+sub = '"Daniel"'
 
-t = time()
-tiempo = ctime(t)
-print(tiempo)
+
+def generate_random256_bit_key():
+    random_key = os.urandom(32)
+    base64url_key = base64.urlsafe_b64encode(random_key).rstrip(b'=').decode('ascii')   
+    return base64url_key
--- src/utils.py
+++ src/utils.py
@@ -2,38 +2,63 @@ import sdjwt
 import token_settings as settings
 from time import time, ctime
 import base64
+import hmac
+import hashlib
 
+def removing_whitespaces_pair_kv(input_str):
+    inside_string = False
+    compact_result = ""
+    
+    for char in input_str:
+        if char == "'":  
+            inside_string = not inside_string
+            compact_result += '"'
+        elif char in [' ', '\n', '\t'] and not inside_string:
+            continue  
+        else:
+            compact_result += char
+    return compact_result
 
-
-def encode_base64(string):
+def encode_base64url(string):
     string_bytes = string.encode("ascii")
     base64_bytes = base64.b64encode(string_bytes)
     base64_string = base64_bytes.decode("ascii")
-    return base64_string
+    base64url_string = base64_string.replace("+", "-").replace("/", "_").rstrip("=")    
+    return base64url_string
 
 def encode64_header(header):
-    alg_str = "alg:" + str(header.alg)
-    typ_str = "typ:" + str(header.typ)
-    header_str = alg_str + typ_str
-    return encode_base64(header_str)
+    alg_str = '{"alg":' + header.alg + ','
+    typ_str = '"typ":' + str(header.typ)
+    header_str = alg_str + typ_str + '}'
+    return encode_base64url(header_str)
 
 def encode64_payload(payload):
-    iss_str = "iss:" + str(payload.iss)
-    exp_str = "exp:" + str(payload.exp)
-    sub_str = "sub:" + str(payload.sub)
-    aud_str = "aud:" + str(payload.aud)
-    claims_str = str(payload.claims).replace(' ', '').replace(',', '').replace('{', '').replace('}', '').replace("'", '')
-    payload_str = iss_str + exp_str + sub_str + aud_str + claims_str
-    return encode_base64(payload_str)
-
+    iss_str = '{"iss":' + str(payload.iss) + ','
+    exp_str = '"exp":' + str(payload.exp) + ','
+    sub_str = '"sub":' + str(payload.sub) + ','
+    aud_str = '"aud":' + str(payload.aud) + ','
+    draft_claims = removing_whitespaces_pair_kv(str(payload.claims))
+    claims_str = draft_claims.replace("'", '"').replace('{', '').replace('}', '')
+    payload_str = iss_str + exp_str + sub_str + aud_str + claims_str + '}'
+    return encode_base64url(payload_str)
 
 def create_signature(enc_header, enc_payload):
-    pass
-
-def build_SDJWT():
-    pass
+    to_be_signed = enc_header + '.' + enc_payload
+    secret_key = settings.generate_random256_bit_key()
+    print("Secret:", secret_key)
+    signature = hmac.new(
+        key=secret_key.encode('utf-8'),
+        msg=to_be_signed.encode('utf-8'),  
+        digestmod=hashlib.sha256  
+    ).digest()
+      
+    signature_base64url = base64.urlsafe_b64encode(signature).rstrip(b'=').decode('utf-8')
     
+    return signature_base64url
 
+def build_SDJWT(draft_sdjwt):
+    return draft_sdjwt.header + '.' + draft_sdjwt.payload + '.' + draft_sdjwt.signature
+    
 def parse_data_to_SDJWT(filepath):
     t = time()
     claims_dic = dict()
@@ -45,25 +70,18 @@ def parse_data_to_SDJWT(filepath):
             claims_dic[claim[0]] = claim[1]
 
     header = sdjwt.Header(settings.alg, settings.typ)
-    payload = sdjwt.Payload(ctime(t), 12345678, settings.sub, settings.aud, claims_dic)
+    payload = sdjwt.Payload(12345678, 12345678, settings.sub, settings.aud, claims_dic)
 
     enc_header = encode64_header(header)
     enc_payload = encode64_payload(payload)
     signature = create_signature(enc_header, enc_payload)
     draft_sdjwt = sdjwt.SD_JWT(enc_header, enc_payload, signature)
     sd_jwt = build_SDJWT(draft_sdjwt)
-
     return sd_jwt
        
-
-
 
 def parse_SDJWT_to_JSON(sd_jwt):
     pass
 
-parse_data_to_SDJWT("data/text/sample1.txt")
-
-dict1 = {'one':1, 'two':2}
-str1 = str(dict1)
-str2 = str1.replace(' ', '').replace(',', '').replace('{', '').replace('}', '').replace("'", '')
-print(str2)
+jwt = parse_data_to_SDJWT("data/text/sample1.txt")
+print(jwt)
